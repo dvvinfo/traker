0b597ecfab039f44fff5128e221884011c56928d
dvvinfo <dvvinfo@mail.ru>
2023-09-05 16:28:41 +0300

    added an activity page

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,53 @@
+<template>
+  <li class="flex flex-col gap-2 p-4">
+    <div class="flex items-center gap-2">
+      <AppButton>
+        <TrashIcon class="h-8" />
+      </AppButton>
+      <span class="truncate text-xl">{{ activity }}</span>
+    </div>
+    <div class="">
+      <AppSelect
+        class="font-mono"
+        :select="secondToComplete"
+        placeholder="h:mm"
+        :options="periodSelectOptions"
+        @select="secondToComplete = $event"
+      />
+    </div>
+  </li>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { TrashIcon } from '@heroicons/vue/24/outline'
+import AppButton from './ui/AppButton.vue'
+import AppSelect from './ui/AppSelect.vue'
+
+defineProps ({
+  activity: {
+    required: true,
+    type: String
+  }
+})
+
+const secondToComplete = ref(null)
+
+const periodSelectOptions = [
+  {
+    value: 15,
+    label: '0:15'
+  },
+  {
+    value: 30,
+    label: '0:30'
+  },
+
+  {
+    value: 15,
+    label: '0:45'
+  }
+]
+</script>
+
+<style scoped></style>
--- src/views/ActivitiesView.vue
+++ src/views/ActivitiesView.vue
@@ -1,9 +1,18 @@
 <template>
-    <TheLayout> Activities </TheLayout>
-  </template>
-  
-  <script setup>
-  import TheLayout from '../components/TheLayout.vue'
-  </script>
-  
-  <style scoped></style>
+  <TheLayout>
+    <div class="">
+      <ul class="divide-y">
+        <ActivityItem v-for="activity in activities" :key="activity" :activity="activity" />
+      </ul>
+    </div>
+  </TheLayout>
+</template>
+
+<script setup>
+import TheLayout from '../components/TheLayout.vue'
+import ActivityItem from '../components/ActivityItem.vue'
+
+const activities = ['Coding', 'Reading', 'Trading']
+</script>
+
+<style scoped></style>
